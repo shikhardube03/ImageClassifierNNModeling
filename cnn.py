2026953a2a0138e951f8fb5e2bcf1e9692784481
shikhardube03 <shikhardube2003@gmail.com>
2023-05-04 17:16:23 -0500

    added implementation for CNN and began testing

--- cnn.py
+++ cnn.py
@@ -3,7 +3,8 @@ import pickle
 import matplotlib.pyplot as plt
 from math import sqrt, ceil
 import cv2
-from timeit import default_timer as timer
+import time
+from scipy.special import cross_entropy
 
 def flatten(x):
     """
@@ -11,6 +12,17 @@ def flatten(x):
     and the second dimension is the flattened feature vector of each example in the batch.
     """
     return np.reshape(x, (x.shape[0], -1))
+
+def relu_grad(x):
+    grad = np.zeros(x.shape)
+    grad[x > 0] = 1
+    return grad
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
 class CNN:
     def __init__(self, num_layers):
@@ -97,3 +109,91 @@ def max_pool(x, size=2, stride=2):
             x_slice = x[:, :, h_start:h_end, w_start:w_end]
             out[:, :, i, j] = np.max(x_slice, axis=(2, 3))
     return out
+
+class Train:
+    def __init__(self, cnn):
+        self.cnn = cnn
+        self.loss = None
+
+    def train(self, x_train, y_train, learning_rate=0.001, num_epochs=10, batch_size=32):
+        num_examples = x_train.shape[0]
+        num_batches = int(num_examples / batch_size)
+
+        for epoch in range(num_epochs):
+            epoch_loss = 0
+            start_time = time.time()
+
+            for batch in range(num_batches):
+                batch_start = batch * batch_size
+                batch_end = (batch + 1) * batch_size
+                x = x_train[batch_start:batch_end]
+                y = y_train[batch_start:batch_end]
+
+                # Forward pass
+                y_pred = self.cnn.forward(x)
+
+                # Compute loss
+                self.loss = cross_entropy(y_pred, y)
+                epoch_loss += self.loss
+
+                # Backward pass
+                grad = y_pred - y
+                for layer in reversed(self.cnn.fc_layers):
+                    grad = grad * relu_grad(layer.last_input)
+                    grad = layer.backward(layer.last_input, grad, learning_rate)
+                grad = flatten(grad)
+                for layer in reversed(self.cnn.conv_layers):
+                    grad = grad.reshape(layer.last_input_shape)
+                    grad = grad * relu_grad(layer.last_input)
+                    grad = layer.backward(layer.last_input, grad, learning_rate)
+
+            epoch_loss /= num_batches
+
+            print("Epoch: {:3d} | Loss: {:.6f} | Time: {:.2f}s".format(epoch+1, epoch_loss, time.time()-start_time))
+
+class LoadData:
+    def __init__(self, data_files):
+        self.data = []
+        self.labels = []
+        self.len_samples = 0
+        for file in data_files:
+            unp = unpickle(file)
+            self.data += unp[b'data']
+            self.labels += unp[b'labels']
+            self.len_samples += len(unp[b'data'])
+            
+        self.labels = np.array(self.labels)
+        self.data = np.array(self.data)
+
+    def __getitem__(self, idx):
+        temp = self.data[idx]
+        label = self.labels[idx]
+
+        image = []
+        for i in range(32): #row
+            row = []
+            for j in range(32):
+                #first red, second green, third blue
+                color = (temp[(i*32)+j], temp[(i*32)+j+1024], temp[(i*32)+j+1024+1024])
+                row.append(color)
+            image.append(row)
+
+        image = np.array(image)
+        return image, label
+
+
+#Testing Implementation below: 
+
+traindata = ["cifar10_batches/data_batch_1", "cifar10_batches/data_batch_2", "cifar10_batches/data_batch_3", "cifar10_batches/data_batch_4", "cifar10_batches/data_batch_5"]
+xtrain = []
+ytrain = []
+dataset = LoadData(traindata)
+for image, label in iter(traindata):
+    xtrain.append(image)
+    ytrain.append(label)
+
+testcnn = CNN(3)
+trainedCNN = Train(testcnn)
+trainedCNN.train(xtrain, ytrain)
+
+
